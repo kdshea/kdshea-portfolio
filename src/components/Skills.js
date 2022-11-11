d9672002b68a2f5bc22f7a3181156bfd6a07c705
Kate Shea <daishea@gmail.com>
2022-11-11 13:48:34 +0530

    Added icons for skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,88 +6,76 @@ const Skills = () => {
         <h2>Skills</h2>
         <ul className="skill-icons">
           <li className="cell">
-            {/* Skill icon i tag here */}
+            <i className="devicon-html5-plain"></i>
             <span>HTML 5</span>
           </li>
           <li className="cell">
-            {/* Skill icon i tag here */}
+            <i className="devicon-css3-plain"></i>
             <span>CSS3</span>
           </li>
           <li className="cell">
-            {/* Skill icon i tag here */}
+            <i className="devicon-sass-original"></i>
             <span>Sass</span>
           </li>
           <li className="cell">
-            {/* Skill icon i tag here */}
+            <i className="devicon-bootstrap-plain"></i>
             <span>Bootstrap</span>
           </li>
           <li className="cell">
-            {/* Skill icon i tag here */}
+            <i className="devicon-javascript-plain"></i>
             <span>JavaScript</span>
           </li>
           <li className="cell">
-            {/* Skill icon i tag here */}
+            <i className="devicon-react-original"></i>
             <span>React</span>
           </li>
           <li className="cell">
-            {/* Skill icon i tag here */}
+            <i className="devicon-express-original"></i>
             <span>Express</span>
           </li>
           <li className="cell">
-            {/* Skill icon i tag here */}
+            <i className="devicon-nodejs-plain"></i>
             <span>Node.js</span>
           </li>
           <li className="cell">
-            {/* Skill icon i tag here */}
+            <i className="devicon-mongodb-plain"></i>
             <span>MongoDB</span>
           </li>
           <li className="cell">
-            {/* Skill icon i tag here */}
+            <i className="devicon-python-plain"></i>
             <span>Python</span>
           </li>
           <li className="cell">
-            {/* Skill icon i tag here */}
+            <i className="devicon-django-plain"></i>
             <span>Django</span>
           </li>
           <li className="cell">
-            {/* Skill icon i tag here */}
-            <span>SQL</span>
-          </li>
-          <li className="cell">
-            {/* Skill icon i tag here */}
+            <i className="devicon-postgresql-plain"></i>
             <span>PostgreSQL</span>
           </li>
           <li className="cell">
-            {/* Skill icon i tag here */}
-            <span>Insomnia</span>
-          </li>
-          <li className="cell">
-            {/* Skill icon i tag here */}
+            <i className="devicon-github-original"></i>
             <span>GitHub</span>
           </li>
           <li className="cell">
-            {/* Skill icon i tag here */}
+            <i className="devicon-github-original"></i>
             <span>Heroku</span>
           </li>
           <li className="cell">
-            {/* Skill icon i tag here */}
+            <i className="devicon-firebase-plain"></i>
             <span>Firebase</span>
           </li>
           <li className="cell">
-            {/* Skill icon i tag here */}
+            <i className="devicon-yarn-plain"></i>
             <span>Yarn</span>
           </li>
           <li className="cell">
-            {/* Skill icon i tag here */}
-            <span>NPM</span>
+            <i className="devicon-npm-original-wordmark"></i>
+            <span>npm</span>
           </li>
           <li className="cell">
-            {/* Skill icon i tag here */}
+            <i className="devicon-figma-plain"></i>
             <span>Figma</span>
-          </li>
-          <li className="cell">
-            {/* Skill icon i tag here */}
-            <span>Adobe Lightroom</span>
           </li>
         </ul>
       </div>
